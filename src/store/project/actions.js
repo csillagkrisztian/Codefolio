fb2637eb44067e4e74e0176bcefc33d970ba0ecd
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-25 14:12:22 +0200

    added new reducer

---
+++ src/store/project/actions.js
@@ -0,0 +1,32 @@
+export const getProject = () => {
+  return async (dispatch, getState) => {
+    // get token from the state
+    const token = selectToken(getState());
+
+    // if we have no token, stop
+    if (token === null) return;
+
+    dispatch(appLoading());
+    try {
+      // if we do have a token,
+      // check wether it is still valid or if it is expired
+      const response = await axios.get(`${apiUrl}/me`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      // token is still valid
+      dispatch(tokenStillValid(response.data));
+      dispatch(appDoneLoading());
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.message);
+      } else {
+        console.log(error);
+      }
+      // if we get a 4xx or 5xx response,
+      // get rid of the token by logging out
+      dispatch(logOut());
+      dispatch(appDoneLoading());
+    }
+  };
+};
