76824a771afe8fd477e8664a39fb942e52205b82
csillagkrisztian <krisztian.csillag93@gmail.com>
2020-08-25 10:13:55 +0200

    added the myprofile page styling with hardcoded data

--- src/pages/MyProfilePage/MyProfilePage.js
+++ src/pages/MyProfilePage/MyProfilePage.js
@@ -1,5 +1,90 @@
 import React from "react";
+import { Grid, Typography, Button } from "@material-ui/core";
+import Carousel from "react-material-ui-carousel";
+import Item from "../../components/CarouselItem/CarouselItem";
+import EditIcon from "@material-ui/icons/Edit";
 
 export default function MyProfilePage() {
-  return <div>MyProfilePage</div>;
+  const user = {
+    name: "rambo",
+    ghLink: "github@rambo.com",
+    liLink: "linkeding.com/rambo",
+    profileImg:
+      "https://qph.fs.quoracdn.net/main-qimg-1694bca506b96e0cb542a000a947bdc2.webp",
+  };
+
+  const projects = [
+    {
+      projectName: "I made an app that can count beans",
+      feLink: "beancounter-frontend.com",
+      beLink: "beancounter-backend.com",
+      projectImg:
+        "https://cdn.loveandlemons.com/wp-content/uploads/2020/03/beans.jpg",
+      ytUrl: "youtube.com/beancounter",
+      projectDes:
+        "My mother is very angry at me every time I get a bad grade in Math, and forces me to count beans, the jokes on her, if she wants to know if it's correct, she has to count herself",
+      userId: 3,
+    },
+    {
+      projectName: "An app that does your math homework",
+      feLink: "mathsux-frontend.com",
+      beLink: "mathsux-backend.com",
+      projectImg: "https://i.ytimg.com/vi/Kp2bYWRQylk/maxresdefault.jpg",
+      ytUrl: "youtube.com/mathsux",
+      projectDes:
+        "I only like math if it's in python, this app solves my problems, literally",
+      userId: 1,
+    },
+    {
+      projectName: "Hot single men in your area",
+      feLink: "hotsinglemen-frontend.com",
+      beLink: "hotsinglemen-backend.com",
+      projectImg:
+        "https://qph.fs.quoracdn.net/main-qimg-1694bca506b96e0cb542a000a947bdc2.webp",
+      ytUrl: "youtube.com/hotsinglemen",
+      projectDes:
+        "I don't like to see only hot single ladies in my area, as a woman I want to see hot single men instead",
+      userId: 2,
+    },
+  ];
+  return (
+    <div className="root">
+      <Grid container justify="center" spacing={3}>
+        <Grid className="profile" item xs={6}>
+          <br />
+          <h1>
+            {user.name}
+
+            <EditIcon />
+          </h1>
+          <img className="profile-main-image" src={user.profileImg}></img>
+          <Typography>
+            Github:
+            <br />
+            {user.ghLink}
+            <EditIcon />
+          </Typography>
+          <Typography>
+            LinkedIn:
+            <br />
+            {user.liLink}
+            <EditIcon />
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Carousel className="carousel" autoPlay={false}>
+            {projects.map(({ projectImg, projectDes }, id) => {
+              return (
+                <Item
+                  key={id + 1}
+                  projectImg={projectImg}
+                  description={projectDes}
+                />
+              );
+            })}
+          </Carousel>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
