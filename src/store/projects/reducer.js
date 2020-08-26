511a034527bd5dd15f91b090ea1e6f304aff77d3
myrinw <myrin.w@outlook.com>
2020-08-26 14:00:52 +0200

    test

--- src/store/projects/reducer.js
+++ src/store/projects/reducer.js
@@ -8,15 +8,7 @@ const initialState = {
 };
 
 export default function postsReducer(state = initialState, action) {
-<<<<<<< HEAD
-    switch (action.type) {
-        case "ADD_PROJECT": {
-            return {
-                ...state,
-                postViewed: action.payload,
-            };
-        }
-=======
+
   switch (action.type) {
     case "DELETE RESOURCES": {
       return { ...state, projectToBe: initialState.projectToBe };
@@ -37,33 +29,32 @@ export default function postsReducer(state = initialState, action) {
         },
       };
     }
-    
 
->>>>>>> 3e6d9866f05488cf5870f3ecded45bd3a155257c
-        case "storePosts": {
-            return {
-                ...state,
-                loading: false,
-                posts: [...action.payload]
-            }
-        }
-        case "searchedPost": {
-            return {
-                ...state,
-                searchPost: [...action.payload]
-            }
-        }
-        case "emptySearch": {
-            return {
-                ...state,
-                searchPost: [],
-            }
-        }
-        default: {
-            return state
-        }
 
+    case "storePosts": {
+      return {
+        ...state,
+        loading: false,
+        posts: [...action.payload]
+      }
     }
+    case "searchedPost": {
+      return {
+        ...state,
+        searchPost: [...action.payload]
+      }
+    }
+    case "emptySearch": {
+      return {
+        ...state,
+        searchPost: [],
+      }
+    }
+    default: {
+      return state
+    }
+
+  }
 }
 
 
