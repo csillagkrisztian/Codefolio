5aed6294f902af9911718e27d21c96628e369b18
Atieh Hamidi <hamidiatiye@gmail.com>
2020-08-27 16:44:23 +0200

    changed comment component add sender and photo of sender

--- src/store/projects/selector.js
+++ src/store/projects/selector.js
@@ -1,6 +1,5 @@
-
-export const projects = state => state.projects.posts;
-export const searchResults = state => state.projects.searchPost;
+export const projects = (state) => state.projects.posts;
+export const searchResults = (state) => state.projects.searchPost;
 
 export const selectProjectViewed = (state) => {
   console.log(state.projects);
@@ -9,4 +8,3 @@ export const selectProjectViewed = (state) => {
 
 export const selectProjectToBeResources = (state) =>
   state.projects.projectToBe.resources;
-
